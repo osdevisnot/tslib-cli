e6de247e3d646f4042470bf8b6315901645c8232
abhisheksh <abhisheksh@zillowgroup.com>
2019-09-16 17:29:05 -0700

    try determining default values for init, also configure git repo

--- init.js
+++ init.js
@@ -1,16 +1,16 @@
 const ncp = require('ncp');
-const replaceStream = require('replacestream');
+const replace = require('replacestream');
 const pkg = require('./package.json');
 
-const { paths, run, cpy, rename, clean, log, question } = require('./utils');
+const { paths, run, rename, log, question } = require('./utils');
 
 module.exports = () => {
   const dest = process.argv[3];
 
-  const userName = question('May I have your username ?      : ');
-  const userEmail = question('And your email address please ? : ');
-  const packageName = question("What's the package name ?       : ");
-  const description = question('And package descriptions ?      : ');
+  const userName = question('may I have your username ?', 'osdevisnot');
+  const email = question('And your email address please ?', `${userName}@gmail.com`);
+  const packageName = question("What's the package name ?", `@${userName}/${dest}`);
+  const description = question('And package descriptions ?', undefined);
 
   const packageNameSane = packageName.split('/').pop();
 
@@ -20,28 +20,34 @@ module.exports = () => {
     {
       transform: (read, write) =>
         read
-          .pipe(replaceStream('username', userName))
-          .pipe(replaceStream('useremail', userEmail))
-          .pipe(replaceStream('template_full', packageName))
-          .pipe(replaceStream('template', packageNameSane))
-          .pipe(replaceStream('<description>', description))
+          .pipe(replace('username', userName))
+          .pipe(replace('useremail', email))
+          .pipe(replace('template_full', packageName))
+          .pipe(replace('template', packageNameSane))
+          .pipe(replace('<description>', description))
           .pipe(write),
     },
     () => {
+      rename(paths.app(dest, 'gitignore'), paths.app(dest, '.gitignore'));
+
       ['src/template.tsx', 'tests/template.test.tsx'].map(f => {
         rename(paths.app(dest, f), paths.app(dest, f.replace('template', packageNameSane)));
       });
-      rename(paths.app(dest, 'gitignore'), paths.app(dest, '.gitignore'));
 
       log('Done !! Now Installing Dependencies...');
+
       if (process.argv[4] === 'link') {
-        run(`yarn add --prefer-offline --dev @types/jest ${paths.cli('tslib-cli-' + pkg.version + '.tgz')}`, {
-          cwd: paths.app(dest),
-        });
-      } else {
-        run('yarn add --prefer-offline --dev tslib-cli @types/jest', { cwd: paths.app(dest) });
+        pkg.name = paths.cli('tslib-cli-' + pkg.version + '.tgz');
       }
-      ['git init', 'git add .', 'git commit -am "Unleash the Kraken"'].map(cmd => run(cmd, { cwd: paths.app(dest) }));
+      run(`yarn add --prefer-offline --dev ${pkg.name} @types/jest`, { cwd: paths.app(dest) });
+
+      [
+        'git init',
+        'git add .',
+        `git config user.name ${userName}`,
+        `git config user.email ${email}`,
+        'git commit -am "Unleash the Kraken"',
+      ].map(cmd => run(cmd, { cwd: paths.app(dest) }));
     },
   );
 };
